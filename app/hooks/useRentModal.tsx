2d8b9b2007d0e5ec0015c01b087e1fba1b172c5b
KallaneKoe <92024724+KallaneKoe@users.noreply.github.com>
2023-10-22 14:04:39 +0700

    first commit

---
+++ app/hooks/useRentModal.tsx
@@ -0,0 +1,16 @@
+import { register } from "module";
+import { create } from "zustand";
+
+interface RentModalStore {
+  isOpen: boolean;
+  onOpen: () => void;
+  onClose: () => void;
+}
+
+const useRentModal = create<RentModalStore>((set) => ({
+  isOpen: false,
+  onOpen: () => set({ isOpen: true }),
+  onClose: () => set({ isOpen: false }),
+}));
+
+export default useRentModal;
