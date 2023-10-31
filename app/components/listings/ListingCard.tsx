7047b49d6ee25862a049e163c1a4eff1e5c8c774
KallaneKoe <92024724+KallaneKoe@users.noreply.github.com>
2023-10-31 21:38:45 +0700

    first commit

--- app/components/listings/ListingCard.tsx
+++ app/components/listings/ListingCard.tsx
@@ -1,12 +1,16 @@
 "use client";
-import { Listing, Reservation } from "@prisma/client";
-import { SafeReservation, SafeUser, safeListings } from "@/app/types";
-import { useCallback, useMemo } from "react";
-import useCountries from "@/app/hooks/useCountries";
+
+import Image from "next/image";
 import { useRouter } from "next/navigation";
+import { useCallback, useMemo } from "react";
 import { format } from "date-fns";
+
+import useCountries from "@/app/hooks/useCountries";
+import { safeListings, SafeReservation, SafeUser } from "@/app/types";
+
 import HeartButton from "../HeartButton";
 import Button from "../Button";
+import ClientOnly from "../ClientOnly";
 
 interface ListingCardProps {
   data: safeListings;
@@ -63,34 +67,63 @@ const ListingCard: React.FC<ListingCardProps> = ({
 
     return `${format(start, "PP")} - ${format(end, "PP")}`;
   }, [reservation]);
+
   return (
     <div
       onClick={() => router.push(`/listings/${data.id}`)}
       className="col-span-1 cursor-pointer group"
     >
       <div className="flex flex-col gap-2 w-full">
-        <div className="aspect-square w-full relative overflow-hidden rounded-xl">
-          <HeartButton listingId={data.id} currentUser={currentUser} />
+        <div
+          className="
+            aspect-square 
+            w-full 
+            relative 
+            overflow-hidden 
+            rounded-xl
+          "
+        >
+          <Image
+            fill
+            className="
+              object-cover 
+              h-full 
+              w-full 
+              group-hover:scale-110 
+              transition
+            "
+            src={data.imageSrc}
+            alt="Listing"
+          />
+          <div
+            className="
+            absolute
+            top-3
+            right-3
+          "
+          >
+            <HeartButton listingId={data.id} currentUser={currentUser} />
+          </div>
         </div>
+        <div className="font-semibold text-lg">
+          {location?.region}, {location?.label}
+        </div>
+        <div className="font-light text-neutral-500">
+          {reservationDate || data.category}
+        </div>
+        <div className="flex flex-row items-center gap-1">
+          <div className="font-semibold">$ {price}</div>
+          {!reservation && <div className="font-light">night</div>}
+        </div>
+        {onAction && actionLabel && (
+          <Button
+            disabled={disabled}
+            small
+            label={actionLabel}
+            onClick={handleCancel}
+          />
+        )}
       </div>
-      <div className="font-semibold text-lg">
-        {location?.region}, {location?.label}
-      </div>
-      <div className="font-light text-neutral-500">
-        {reservationDate || data.category}
-      </div>
-      <div className="flex flex-row items-center gap-1">
-        <div className="font-semibold">$ {price}</div>
-        {!reservation && <div className="font-light">night</div>}
-      </div>
-      {onAction && actionLabel && (
-        <Button
-          disabled={disabled}
-          small
-          label={actionLabel}
-          onClick={handleCancel}
-        />
-      )}
     </div>
   );
 };
