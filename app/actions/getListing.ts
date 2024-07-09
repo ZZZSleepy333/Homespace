9c329c1814116fe9abe913654c31880ac2870639
KallaneKoe <92024724+KallaneKoe@users.noreply.github.com>
2024-07-09 11:32:05 +0700

    commit

--- app/actions/getListing.ts
+++ app/actions/getListing.ts
@@ -82,10 +82,12 @@ export default async function getListings(params: IListingsParams) {
       },
     });
 
-    const safeListings = listings.map((listing) => ({
-      ...listing,
-      createdAt: listing.createdAt.toISOString(),
-    }));
+    const safeListings = listings.map(
+      (listing: { createdAt: { toISOString: () => any } }) => ({
+        ...listing,
+        createdAt: listing.createdAt.toISOString(),
+      })
+    );
 
     return safeListings;
   } catch (error: any) {
